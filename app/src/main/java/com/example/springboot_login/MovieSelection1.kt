2d9f0276163413b46dccb4f271b87bafd7cf171f
Donnedoni Fillies <donnedonifillies@gmail.com>
2023-05-16 13:22:30 +0200

    added youtube links to the Image Views

--- app/src/main/java/com/example/springboot_login/MovieSelection1.kt
+++ app/src/main/java/com/example/springboot_login/MovieSelection1.kt
@@ -1,9 +1,11 @@
 package com.example.springboot_login
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MovieSelection1 : AppCompatActivity() {
@@ -11,8 +13,29 @@ class MovieSelection1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_selection1)
 
+        val imageView1 = findViewById<ImageView>(R.id.onward)
+        imageView1.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=gn5QmllRCn4&pp=ygUOb253YXJkIHRyYWlsZXI%3D")
+        }
+
+        val imageView2= findViewById<ImageView>(R.id.avatar)
+        imageView2.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=d9MyW72ELq0&pp=ygUQYXZhdGFyIDIgdHJhaWxlcg%3D%3D")
+        }
+        val imageView3 = findViewById<ImageView>(R.id.frozen)
+        imageView3.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=Zi4LMpSDccc&pp=ygUQZnJvemVuIDIgdHJhaWxlcg%3D%3D")
+        }
+        val imageView4 = findViewById<ImageView>(R.id.blackWidow)
+        imageView4.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=ybji16u608U&pp=ygUUYmxhY2sgd2lkb3cgdHJhaWxlciA%3D")
+        }
         this.findViewById<Button>(R.id.next_button).setOnClickListener {
             startActivity(Intent(this, MovieSelection2::class.java))
         }
+    }
+    private fun openYouTubeVideo(videoUrl: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl))
+        startActivity(intent)
     }
 }
